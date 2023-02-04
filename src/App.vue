fb11c03c65b52c85ae9f2827bd8de930259057ee
Jason Harris <jasonharris@Jasons-MBP.attlocal.net>
2023-02-04 10:51:14 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <MainNav />
+</template>
+
+<script>
+import MainNav from '@/components/MainNav.vue';
+
+export default {
+  name: "App",
+  components: {
+    MainNav
+  }
+}
+</script>
+
+
---
+++ src/components/MainNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div class="flex flex-nowrap h-full border-b border-solid border-brand-grey-1 px=8 mx=auto">
+        <a href='/' class="flex h-full items-center text-xl">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Bobo Careers",
+    }
+  }
+</script>
